daa5a48c7af690c97d8d0adcf7e78e1f8427c803
Chris Ortman <chris-ortman@uiowa.edu>
2017-04-18 15:20:05 -0500

    Getting rid of 'base' package
    At one time I was thinking to have a 'base' package that
    contained the rails app and then 2 satellite packages that did
    the running, but that causes me problems with config files because
    habitat only processes the config file when you start a package
    and the base plan would never technically start
    
    So now my plan is to have a sparc request package and hopefully
    a general purpose delayed job package

--- habitat/sparc-request-app/plan.sh
+++ habitat/sparc-request-app/plan.sh
@@ -1,47 +1,58 @@
-pkg_name=sparc-request-app
+# This file is the heart of your application's habitat.
+# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
+
+pkg_name=sparc-request
 pkg_origin=chrisortman
 pkg_version="0.1.0"
 pkg_source="https://github.com/ui-icts/sparc-request/archive/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum="NONE"
+# Overwritten later because we compute it based on the repo
+pkg_shasum="b663cefcbd5fabd7fabb00e6a114c24103391014cfe1c5710a668de30dd30371"
 pkg_deps=(
-  chrisortman/sparc-request
+  core/runit # Maybe not needed, thought i had to use it to get chpst
+  core/cacerts
+  core/glibc
+  core/libffi
+  core/libxml2
+  core/libxslt
+  core/libyaml
+  core/node
+  core/mysql-client
+  chrisortman/ruby
   )
-pkg_exports=(
-  [port]=rails_port
+pkg_build_deps=(
+  core/coreutils
+  core/git
+  core/rsync
+  core/gcc
+  core/make
+  core/curl
+  core/which
 )
-pkg_exposes=(port)
-
-pkg_binds_optional=(
-  [database]="port"
-)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_svc_user="hab"
+pkg_svc_group="$pkg_svc_user"
 
 # Callback Functions
-
+#
 do_begin() {
-  cp -a $PLAN_CONTEXT/../sparc-request-base/config/* $PLAN_CONTEXT/config
-  cp $PLAN_CONTEXT/../sparc-request-base/default.toml $PLAN_CONTEXT/
   return 0
 }
 
-# The default implementation is that the software specified in $pkg_source is
-# downloaded, checksum-verified, and placed in $HAB_CACHE_SRC_PATH/$pkgfilename,
-# which resolves to a path like /hab/cache/src/filename.tar.gz. You should
-# override this behavior if you need to change how your binary source is
-# downloaded, if you are not downloading any source code at all, or if your are
-# cloning from git. If you do clone a repo from git, you must override
-# do_verify() to return 0.
 do_download() {
-  return 0
+  export GIT_SSL_CAINFO="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
+
+  # This is a way of getting the git code that I found in the chef plan
+  build_line "Fake download! Creating archive of latest repository commit from $PLAN_CONTEXT"
+  cd $PLAN_CONTEXT/../..
+  git archive --prefix=${pkg_name}-${pkg_version}/ --output=$HAB_CACHE_SRC_PATH/${pkg_filename} HEAD
+
+  pkg_shasum=$(trim $(sha256sum $HAB_CACHE_SRC_PATH/${pkg_filename} | cut -d " " -f 1))
 }
 
-# The default implementation tries to verify the checksum specified in the plan
-# against the computed checksum after downloading the source tarball to disk.
-# If the specified checksum doesn't match the computed checksum, then an error
-# and a message specifying the mismatch will be printed to stderr. You should
-# not need to override this behavior unless your package does not download
-# any files.
 do_verify() {
-  return 0
+  do_default_verify
+  # return 0
 }
 
 # The default implementation removes the HAB_CACHE_SRC_PATH/$pkg_dirname folder
@@ -51,32 +62,55 @@ do_clean() {
   do_default_clean
 }
 
-# The default implementation extracts your tarball source file into
-# HAB_CACHE_SRC_PATH. The supported archives are: .tar, .tar.bz2, .tar.gz,
-# .tar.xz, .rar, .zip, .Z, .7z. If the file archive could not be found or was
-# not supported, then a message will be printed to stderr with additional
-# information.
 do_unpack() {
-  return 0
+  do_default_unpack
+  # return 0
 }
 
-# There is no default implementation of this callback. At this point in the
-# build process, the tarball source has been downloaded, unpacked, and the build
-# environment variables have been set, so you can use this callback to perform
-# any actions before the package starts building, such as exporting variables,
-# adding symlinks, and so on.
 do_prepare() {
+  build_line "Setting link for /usr/bin/env to 'coreutils'"
+  [[ ! -f /usr/bin/env ]] && ln -s "$(pkg_path_for coreutils)/bin/env" /usr/bin/env
   return 0
 }
 
-# The default implementation is to update the prefix path for the configure
-# script to use $pkg_prefix and then run make to compile the downloaded source.
-# This means the script in the default implementation does
-# ./configure --prefix=$pkg_prefix && make. You should override this behavior
-# if you have additional configuration changes to make or other software to
-# build and install as part of building your package.
 do_build() {
-  return 0
+
+  export CPPFLAGS="${CPPFLAGS} ${CFLAGS}"
+  local _libxml2_dir=$(pkg_path_for libxml2)
+  local _libxslt_dir=$(pkg_path_for libxslt)
+  local _zlib_dir=$(pkg_path_for zlib)
+
+  #export GEM_HOME=${pkg_prefix}/vendor/bundle
+  #export GEM_PATH=${GEM_HOME}
+
+  # don't let bundler split up the nokogiri config string (it breaks
+  # the build), so specify it as an env var instead
+  export NOKOGIRI_CONFIG="--use-system-libraries --with-zlib-dir=${_zlib_dir} --with-xslt-dir=${_libxslt_dir} --with-xml2-include=${_libxml2_dir}/include/libxml2 --with-xml2-lib=${_libxml2_dir}/lib"
+
+  # we control the variable above, and it will be all on one line, and
+  # we need single quotes otherwise the extconf doesn't build the
+  # extension.
+  bundle config build.nokogiri '${NOKOGIRI_CONFIG}'
+
+  # We need to add tzinfo-data to the Gemfile since we're not in an
+  # environment that has this from the OS
+   if ! grep -q 'gem .*tzinfo-data.*' Gemfile; then
+     echo 'gem "tzinfo-data"' >> Gemfile
+   fi
+
+  # If you want rails console to work you need to
+  # provide an implementation of readline
+   if ! grep -q 'gem .*rb-readline.*' Gemfile; then
+     echo 'gem "rb-readline"' >> Gemfile
+   fi
+
+  bundle install --without test development --jobs 2 --retry 5 --path vendor/bundle --binstubs
+
+  # Some bundle files when they install have permissions that don't
+  # allow the all user to read them, but because we are running as
+  # root right now for building, but as 'hab' or someone else when the
+  # package installs we need to make sure we can read the files
+  chmod -R a+rx vendor/bundle
 }
 
 # The default implementation runs nothing during post-compile. An example of a
@@ -96,7 +130,39 @@ do_check() {
 # specific directories in your package, or installing pre-built binaries into
 # your package.
 do_install() {
-  return 0
+
+  # At this point my current directory is something
+  # like /hab/cache/src/sparc-request-0.1.0
+  # this the HAB CACHE SRC PATH or some such 
+  # and in this directory I have a copy of my
+  # rails app because this is where do_unpack would have
+  # extracted my archive to.
+  # The job of this task then is to get the files out of there
+  # and put them someplace _useful_
+  # Now the rails sample where I copied all this from
+  # copies the files to pkg_prefix/release which I think can be 
+  # thought of in the same vein as capistrano's releases folder?
+  # so in order to not have new files overwriting existing files you
+  # need something similar which for habitat is the package path because
+  # that is all versioned out.
+  # EDIT: I changed the cp -r to cp -a cuz maybe that's better
+  # since I'm setting my user to hab up above anyway?
+  echo "Copying current files to ${pkg_prefix}"
+  mkdir -p "${pkg_prefix}/static/release"
+  cp -a . "${pkg_prefix}/static/release"
+
+
+  # This seems to be some habitat stuff that you 
+  # just need to do?
+  for binstub in ${pkg_prefix}/static/release/bin/*; do
+    build_line "Setting shebang for ${binstub} to 'ruby'"
+    [[ -f $binstub ]] && sed -e "s#/usr/bin/env ruby#$(pkg_path_for ruby)/bin/ruby#" -i "$binstub"
+  done
+
+  if [[ $(readlink /usr/bin/env) = "$(pkg_path_for coreutils)/bin/env" ]]; then
+    build_line "Removing the symlink we created for '/usr/bin/env'"
+    rm /usr/bin/env
+  fi
 }
 
 # The default implementation is to strip any binaries in $pkg_prefix of their
@@ -105,7 +171,7 @@ do_install() {
 # subdirectories might also need to be stripped, or whether you do not want the
 # binaries stripped at all.
 do_strip() {
-  return 0
+  do_default_strip
 }
 
 # There is no default implementation of this callback. This is called after the
