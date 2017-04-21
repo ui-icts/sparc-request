56325971fbd328712854a312a0644fea15da7e1f
Chris Ortman <chris-ortman@uiowa.edu>
2017-04-21 10:52:55 -0500

    Don't use specific hab version in vagrant script

--- script/vagrant/hab-install.sh
+++ script/vagrant/hab-install.sh
@@ -5,8 +5,8 @@ sudo adduser --group hab
 sudo useradd -g hab hab
 
 # Install hab
-rm -f hab-0.20.*.tar.gz
+rm -f hab-0.*.*.tar.gz
 curl -O -J -L https://api.bintray.com/content/habitat/stable/linux/x86_64/hab-%24latest-x86_64-linux.tar.gz?bt_package=hab-x86_64-linux
-tar -xzf hab-0.20.*.tar.gz
+tar -xzf hab-0.*.*.tar.gz
 sudo mv hab-**/hab /usr/local/bin
 sudo chmod a+x /usr/local/bin/hab
