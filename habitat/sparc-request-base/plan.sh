9e2463e456d36e2946dd931bafded7dcb7f8f0b8
Chris Ortman <chris-ortman@uiowa.edu>
2018-10-17 08:56:21 -0500

    Update plan, gems, and ruby version

--- habitat/sparc-request-base/plan.sh
+++ habitat/sparc-request-base/plan.sh
@@ -3,7 +3,7 @@
 
 pkg_name=sparc-request
 pkg_origin=chrisortman
-pkg_version="3.2.0"
+pkg_version="3.3.0"
 pkg_source="https://github.com/ui-icts/sparc-request/archive/${pkg_name}-${pkg_version}.tar.bz2"
 # Overwritten later because we compute it based on the repo
 pkg_shasum="b663cefcbd5fabd7fabb00e6a114c24103391014cfe1c5710a668de30dd30371"
@@ -16,7 +16,7 @@ pkg_deps=(
   core/curl
   core/rsync
 
-  core/ruby/2.4.2
+  core/ruby
   core/bundler
   chrisortman/eye
 
