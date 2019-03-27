d3df75f578eae2f7be1eb1229ca5b8fbc4262a6a
Chris Ortman <chris-ortman@uiowa.edu>
2019-03-27 15:26:21 -0500

    Serve the prawn PDF

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -152,7 +152,11 @@ class ProtocolsController < ApplicationController
       }
       format.pdf {
         response.headers['Content-Disposition'] = "attachment; filename=\"(#{@protocol.id}).pdf\""
-        pdf = Prawn::Document.new
+        pdf = Prawn::Document.new(:page_layout => :landscape)
+        generator = CostAnalysis::Generator.new
+        generator.protocol = @protocol
+        generator.to_pdf(pdf)
+        send_data pdf.render, filename: "Cost Analysis (#{@protocol.id}).pdf", type: "application/pdf", disposition: "inline"
       }
       format.html { render layout: false }
     end
