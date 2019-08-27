8e0b8920a0e8aaab7c6730371191fd2df1d2928d
Chris Ortman <chris-ortman@uiowa.edu>
2019-08-27 10:42:06 -0500

    Update proper_yml transform

--- script/configurious/proper_yml_transform.rb
+++ script/configurious/proper_yml_transform.rb
@@ -1,7 +1,17 @@
 update 'en' do
   update 'proper' do
+    update 'footer' do
+      change 'sctr', to: 'ICTS'
+      change 'sctr_url', to: 'https://icts.uiowa.edu'
+    end
     update 'catalog' do
       change 'no_description', to: ''
+    end
+
+    update 'right_navigation' do
+      update 'faqs' do
+        change 'answer_6', to: 'Please contact ICTS at iCartHelp@healthcare.uiowa.edu.'
+      end
     end
   end
 end
