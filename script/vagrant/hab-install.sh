eb5ac79fe3f0473f7cd1dfbd10d1e3599da1ac58
Chris Ortman <chris-ortman@uiowa.edu>
2019-08-28 09:14:42 -0500

    Pin hab version

--- script/vagrant/hab-install.sh
+++ script/vagrant/hab-install.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 #Set up hab user & group
-sudo adduser --group hab
-sudo useradd -g hab hab
+# sudo adduser --group hab
+# sudo useradd -g hab hab
 
 # Install hab
-rm -f hab-0.*.*.tar.gz
-curl -O -J -L https://api.bintray.com/content/habitat/stable/linux/x86_64/hab-%24latest-x86_64-linux.tar.gz?bt_package=hab-x86_64-linux
-tar -xzf hab-0.*.*.tar.gz
-sudo mv hab-**/hab /usr/local/bin
-sudo chmod a+x /usr/local/bin/hab
+curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash -v 0.79.1
+# tar -xzf hab-0.*.*.tar.gz
+# sudo mv hab-**/hab /usr/local/bin
+# sudo chmod a+x /usr/local/bin/hab
 
 
-# Mimic some of the way boxes are organized for chef
-
-## This is where I store my user.toml files by default
-mkdir -p /etc/configs
